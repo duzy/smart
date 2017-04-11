58313b084f23910aabb85c10d4da0249b54269ae
Duzy Chan <code@duzy.info>
2017-04-11 17:47:04 +0800

    add base64 and read-file

--- runtime/builtins.go
+++ runtime/builtins.go
@@ -10,6 +10,9 @@ import (
         //"github.com/duzy/smart/token"
         "github.com/duzy/smart/types"
         "github.com/duzy/smart/values"
+        "io/ioutil"
+        "bytes"
+        "encoding/base64"
         //"fmt"
 )
 
@@ -20,6 +23,23 @@ var (
                 `return`:       builtinReturn,
                 `lit`:          builtinLit,
                 //`run`:          builtinRun,
+
+                `read-file`:    builtinReadFile,
+
+                `encode-base64`:  builtinEncodeBase64,
+                `decode-base64`:  builtinDecodeBase64,
+
+                /* TODO:
+                `encode-base32`
+                `decode-base32`
+                `encode-json`
+                `decode-json`
+                `encode-xml`
+                `decode-xml`
+                `encode-hex`
+                `decode-hex`
+                `encode-csv`
+                `decode-csv` */
         }
 )
 
@@ -74,3 +94,59 @@ func builtinRun(ctx *Context, args... types.Value) (result types.Value, err erro
         }
         return
 } */
+
+func builtinReadFile(ctx *Context, args... types.Value) (res types.Value, err error) {
+        var l []types.Value
+        for _, a := range args {
+                var s []byte
+                if s, err = ioutil.ReadFile(a.String()); err == nil {
+                        l = append(l, values.String(string(s)))
+                } else {
+                        l = append(l, values.None)
+                }
+        }
+        if x := len(l); x == 0 {
+                res = values.None
+        } else if x == 1 {
+                res = l[0]
+        } else {
+                res = values.List(l...)
+        }
+        return
+}
+
+func builtinEncodeBase64(ctx *Context, args... types.Value) (res types.Value, err error) {
+        if len(args) > 0 {
+                buf := new(bytes.Buffer)
+                enc := base64.NewEncoder(base64.StdEncoding, buf)
+                for _, a := range args {
+                        enc.Write([]byte(a.String()))
+                }
+                enc.Close()
+                res = values.String(buf.String())
+        }
+        return
+}
+
+func builtinDecodeBase64(ctx *Context, args... types.Value) (res types.Value, err error) {
+        if len(args) > 0 {
+                var list []types.Value
+                for _, a := range args {
+                        var dat []byte
+                        dat, err = base64.StdEncoding.DecodeString(a.String())
+                        if err == nil {
+                                list = append(list, values.String(string(dat)))
+                        } else {
+                                return
+                        }
+                }
+                if x := len(list); x == 0 {
+                        res = values.None
+                } else if x == 1 {
+                        res = list[0]
+                } else {
+                        res = values.List(list...)
+                }
+        }
+        return
+}
