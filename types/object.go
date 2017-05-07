8e3de5df033bb4c3a9508332b92b4c218df88c65
Duzy Chan <code@duzy.info>
2017-05-07 13:12:22 +0800

    renamed types.Symbol, types.Module

---
+++ types/object.go
@@ -0,0 +1,208 @@
+//
+//  Copyright (C) 2012-2017, Duzy Chan <code@duzy.info>, all rights reserverd.
+//  Use of this source code is governed by a BSD-style license that can be
+//  found in the LICENSE file.
+//
+
+package types
+
+import (
+        "github.com/duzy/smart/token"
+        "fmt"
+)
+
+// Object is a value defined in a scope.
+//
+// TODO: defines ObjInfo to classify objects.
+// 
+type Object interface {
+        Parent() *Scope
+        Project() *Project
+        Name() string
+        Type() Type // the type of the object, differs from the value type
+
+        Call(args... Value) (Value, error)
+
+        Lit() string
+        String() string
+        Integer() int64
+        Float() float64
+
+	// order reflects a package-level object's source order: if object
+	// a is before object b in the source, then a.order() < b.order().
+	// order returns a value > 0 for package-level objects; it returns
+	// 0 for all other objects (including objects in file scopes).
+	order() uint32
+
+	// setParent sets the parent scope of the object.
+	setParent(*Scope)
+
+	// scopePos returns the start position of the scope of this Object
+	scopePos() token.Pos
+
+	// setScopePos sets the start position of the scope for this Object.
+	setScopePos(pos token.Pos) // FIXME: it's not applied
+}
+
+// An object implements the common parts of an Object.
+type object struct {
+        parent *Scope
+        project *Project
+        name string
+        typ Type
+        ord uint32
+        scopos token.Pos
+}
+
+func (obj *object) Parent() *Scope        { return obj.parent }
+func (obj *object) Project() *Project     { return obj.project }
+func (obj *object) Name() string          { return obj.name }
+func (obj *object) Type() Type            { return obj.typ }
+func (obj *object) Lit() string           { return "" }
+func (obj *object) String() string        { return fmt.Sprintf("object ", obj.name) }
+func (obj *object) Integer() int64        { return 0 }
+func (obj *object) Float() float64        { return 0 }
+func (obj *object) Call(a... Value) (Value, error) { return nil, nil }
+func (obj *object) order() uint32         { return obj.ord }
+func (obj *object) scopePos() token.Pos   { return obj.scopos }
+
+func (obj *object) setParent(parent *Scope)   { obj.parent = parent }
+func (obj *object) setOrder(order uint32)     { /*assert(order > 0);*/ obj.ord = order }
+func (obj *object) setScopePos(pos token.Pos) { obj.scopos = pos }
+
+func NewDummy(mod *Project, scope *Scope, name string) Object {
+	return &object{scope, mod, name, Invalid, 0, token.NoPos}
+}
+
+func IsDummy(s Object) bool {
+        _, ok := s.(*object)
+        return ok
+}
+
+func IsDummyValue(s Value) bool {
+        _, ok := s.(*object)
+        return ok
+}
+
+type ProjectName struct {
+        object
+        imported *Project
+        used bool // set if the project was used
+}
+
+// Imported returns the project that was imported.
+// It is distinct from Project(), which is the project
+// containing the import statement.
+func (n *ProjectName) Imported() *Project { return n.imported }
+func (n *ProjectName) String() string  {
+        //return fmt.Sprintf("%s[%p]", n.name, n.imported)
+        return fmt.Sprintf("project %s", n.name)
+}
+
+func NewProjectName(mod *Project, name string, imported *Project) *ProjectName {
+	return &ProjectName{object{nil, mod, name, ProjectNameType, 0, token.NoPos}, imported, false}
+}
+
+// A Const represents a declared constant.
+/* type Const struct {
+        object
+} */
+
+// A Def represents a definition.
+type Def struct {
+        object
+        value Value
+}
+
+func (d *Def) Value() Value    { return d.value }
+func (d *Def) String() string  { return d.name+" = "+d.value.String() }
+func (d *Def) Set(v Value)     { d.value = v }
+func (d *Def) Call(a... Value) (Value, error) { return d.value, nil }
+
+func NewDef(mod *Project, name string, value Value) *Def {
+	return &Def{object{nil, mod, name, DefineType, 0, token.NoPos}, value}
+}
+
+// A Builtin represents a built-in function.
+// Builtins don't have a valid type.
+type Builtin struct {
+        object
+        f BuiltinFunc
+}
+
+func (p *Builtin) Call(a... Value) (Value, error) { return p.f(a...) }
+func (p *Builtin) String() string { return fmt.Sprintf("builtin %v", p.name) }
+
+func NewBuiltin(name string, f BuiltinFunc) *Builtin {
+        return &Builtin{object{
+                parent: nil, 
+                project: nil, 
+                name: name, 
+                typ: BuiltinType,
+                ord: 0,
+                scopos: token.NoPos,
+        }, f}
+}
+
+type RuleEntryClass int
+
+const (
+        GeneralRuleEntry RuleEntryClass = 1<<iota
+        FileRuleEntry
+        PatternRuleEntry
+        PatternFileRuleEntry
+)
+
+var ruleEntryClassNames = []string{
+        GeneralRuleEntry:     "GeneralRuleEntry",
+        FileRuleEntry:        "FileRuleEntry",
+        PatternRuleEntry:     "PatternRuleEntry",
+        PatternFileRuleEntry: "PatternFileRuleEntry",
+}
+
+func (c RuleEntryClass) String() string {
+        var i = int(c)
+        if 0 < i && i < len(ruleEntryClassNames) {
+                return ruleEntryClassNames[i]
+        }
+        return fmt.Sprintf("RuleEntryClass(%d)", i)
+}
+
+// RuleEntry represents a declared rule entry.
+type RuleEntry struct {
+        object
+        kind RuleEntryClass
+        program Program
+        stem string // only applied for PatternRuleEntry
+}
+
+func (entry *RuleEntry) String() string { return entry.name }
+func (entry *RuleEntry) Stem() string { return entry.stem }
+
+func (entry *RuleEntry) Kind() RuleEntryClass { return entry.kind }
+
+// RuleEntry.Program returns the rule program.
+func (entry *RuleEntry) Program() Program { return entry.program }
+
+// RuleEntry.Execute executes the rule program only if the target
+// is outdated.
+//
+// TODO: merge Execute and Call, make RuleEntry behaves like a Def
+// 
+func (entry *RuleEntry) Call(a... Value) (result Value, err error) {
+        if entry.program != nil {
+                result, err = entry.program.Execute(entry, a, false)
+        }
+        return
+}
+
+func NewRuleEntry(kind RuleEntryClass, name string) (entry *RuleEntry) {
+        return &RuleEntry{
+                object{
+                        nil, nil, name, RuleEntryType, 
+                        0, token.NoPos,
+                },
+                kind, nil, 
+                "",
+        }
+}
---
+++ types/project.go
@@ -0,0 +1,247 @@
+//
+//  Copyright (C) 2012-2017, Duzy Chan <code@duzy.info>, all rights reserverd.
+//  Use of this source code is governed by a BSD-style license that can be
+//  found in the LICENSE file.
+//
+package types
+
+import (
+        //"github.com/duzy/smart/token"
+        "path/filepath"
+        "strings"
+        "fmt"
+)
+
+// Pattern
+type Pattern interface {
+        Value
+        Entry(stem string) *RuleEntry
+        Match(s string) (matched bool, stem string)
+}
+
+type pattern struct {
+        parent *Scope
+        project *Project
+        program Program
+}
+
+func (p *pattern) Type() Type        { return Invalid }
+func (p *pattern) Integer() int64    { return 0 }
+func (p *pattern) Float() float64    { return 0 }
+func (p *pattern) Program() Program  { return p.program }
+func (p *pattern) entry(name, stem string) (entry *RuleEntry) {
+        var kind = PatternRuleEntry
+        if p.project != nil && p.project.IsFile(name) {
+                kind = PatternFileRuleEntry
+        }
+        entry = NewRuleEntry(kind, name)
+        entry.parent = p.parent
+        entry.project = p.project
+        entry.program = p.program
+        entry.stem = stem
+        return
+}
+
+type PercentPattern struct {
+        pattern
+        prefix Value
+        suffix Value
+}
+
+func NewPercentPattern(m *Project, prefix, suffix Value) Pattern {
+        return &PercentPattern{pattern:pattern{project:m}, prefix:prefix, suffix:suffix }
+}
+
+func (p *PercentPattern) Lit() string { return p.String() }
+func (p *PercentPattern) String() (s string) {
+        if p.prefix != nil {
+                s = p.prefix.String()
+        }
+        s += "%"
+        if p.suffix != nil {
+                s += p.suffix.String()
+        }
+        return
+}
+func (p *PercentPattern) Match(s string) (matched bool, stem string) {
+        /*
+        if pp, _ := p.prefix.(*PercentPattern); pp != nil {
+        }
+        if pp, _ := p.suffix.(*PercentPattern); pp != nil {
+        } */
+        if prefix := p.prefix.String(); prefix == "" || strings.HasPrefix(s, prefix) {
+                if suffix := p.suffix.String(); suffix == "" || strings.HasSuffix(s, suffix) {
+                        if a, b := len(prefix), len(s)-len(suffix); a < b {
+                                matched, stem = true, s[a:b]
+                        }
+                }
+        }
+        return
+}
+
+func (p *PercentPattern) Entry(stem string) (entry *RuleEntry) {
+        name := p.prefix.String() + stem + p.suffix.String()
+        entry = p.entry(name, stem)
+        return
+}
+
+type RegexpPattern struct {
+        pattern
+}
+
+func NewRegexpPattern() Pattern {
+        return &RegexpPattern{}
+}
+
+func (p *RegexpPattern) Lit() string { return p.String() }
+func (p *RegexpPattern) String() (s string) { return "" }
+func (p *RegexpPattern) Match(s string) (matched bool, stem string) {
+        return
+}
+func (p *RegexpPattern) Entry(stem string) (entry *RuleEntry) {
+        return
+}
+
+type Program interface {
+        Scope() *Scope
+        Execute(entry *RuleEntry, args []Value, forced bool) (result Value, err error)
+}
+
+type Project struct {
+	path      string
+	name      string
+        scope     *Scope
+        imports   []*Project
+        uses      []*Use
+
+        exts      map[string][]string
+        files     []string
+
+        // Rule Registry
+        dedicated []*RuleEntry
+        patterns  []Pattern
+}
+
+func (m *Project) Path() string { return m.path }
+func (m *Project) Name() string { return m.name }
+func (m *Project) Scope() *Scope { return m.scope }
+func (m *Project) Imports() []*Project { return m.imports }
+func (m *Project) Uses() []*Use { return m.uses }
+
+func (m *Project) AddImport(o *Project) {
+        m.imports = append(m.imports, o)
+}
+
+func (m *Project) FindImport(name string) (res *Project) {
+        for _, m := range m.imports {
+                if m.Name() == name {
+                        res = m; break
+                }
+        }
+        return
+}
+
+func (m *Project) AddExts(exts map[string][]string) {
+        if m.exts == nil {
+                m.exts = make(map[string][]string)
+        }
+        for k, a := range exts {
+                m.exts[k] = append(m.exts[k], a...)
+        }
+}
+
+func (m *Project) AddFiles(a []string) {
+outter:
+        for _, s := range a {
+                for _, f := range m.files {
+                        if s == f { continue outter }
+                }
+                m.files = append(m.files, s)
+        }
+}
+
+func (m *Project) IsFile(s string) (v bool) {
+        if len(s) > 0 {
+                if m.exts != nil {
+                        if ext := filepath.Ext(s); ext != "" {
+                                if _, v = m.exts[ext[1:]]; v {
+                                        return
+                                }
+                        }
+                }
+                var ss = filepath.Base(s)
+                for _, pat := range m.files {
+                        if strings.ContainsAny(pat, "*?[") {
+                                v, _ = filepath.Match(pat, ss)
+                                //fmt.Printf("IsFile: %v, %v, %v\n", s, pat, v)
+                        } else { 
+                                v = s == pat
+                        }
+                        if v { break }
+                }
+        }
+        return
+}
+
+func (m *Project) EntryClass(name string) (kind RuleEntryClass) {
+        if kind = GeneralRuleEntry; m.IsFile(name) {
+                kind = FileRuleEntry
+        }
+        //fmt.Printf(": %v %v %v\n", name, kind, m.files)
+        return
+}
+
+func (m *Project) Lookup(s string) (entry *RuleEntry) {
+        if sym := m.scope.Lookup(s); sym != nil {
+                entry, _ = sym.(*RuleEntry)
+        }
+        return
+}
+
+func (m *Project) AddPercentPattern(p *PercentPattern, prog Program) {
+        p.parent = m.scope
+        p.project = m
+        p.program = prog
+        m.patterns = append(m.patterns, p)
+}
+
+func (m *Project) MatchPattern(s string) (res Pattern, stem string) {
+        var found bool
+        for _, p := range m.patterns {
+                if found, stem = p.Match(s); found && stem != "" {
+                        res = p
+                }
+        }
+        return
+}
+
+func (m *Project) FindPercentPattern(s string) (res *PercentPattern) {
+        for _, p := range m.patterns {
+                if pp, _ := p.(*PercentPattern); pp != nil && pp.String() == s {
+                        res = pp
+                }
+        }
+        return
+}
+
+func (m *Project) Insert(name string, prog Program) (entry *RuleEntry) {
+        if sym := m.scope.Lookup(name); sym == nil {
+                entry = NewRuleEntry(m.EntryClass(name), name)
+                entry.parent = m.scope
+                entry.project = m
+                m.scope.Insert(entry)
+        } else if entry, _ = sym.(*RuleEntry); entry == nil {
+                panic(fmt.Sprintf("name '%v' already taken\n", sym.Name()))
+        }
+        entry.program = prog
+        //entry.pos = pos // overwrite position
+        m.dedicated = append(m.dedicated, entry)
+        return
+}
+
+func (m *Project) GetDefaultEntry() (entry *RuleEntry) {
+        if len(m.dedicated) > 0 {
+                entry = m.dedicated[0]
+        }
+        return
+}
