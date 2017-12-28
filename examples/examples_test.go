9ce129f131b5508be88f442563823b2459fadc4a
Duzy Chan <code@duzy.info>
2017-12-28 17:57:32 +0800

    Added examples test code

---
+++ examples/examples_test.go
@@ -0,0 +1,14 @@
+//
+//  Copyright (C) 2012-2017, Duzy Chan <code@duzy.info>, all rights reserverd.
+//
+package examples
+
+import (
+        "testing"
+        "fmt"
+        "os"
+)
+
+func TestAll(t *testing.T) {
+        fmt.Fprintf(os.Stderr, "TODO: test all examples\n")
+}
