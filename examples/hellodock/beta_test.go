031600a695b366d1218eb51916da229c71a0b0f7
Duzy Chan <code@duzy.info>
2017-12-29 13:27:26 +0800

    Added hello,hellodock test cases

--- examples/hellodock/beta_test.go
+++ examples/hellodock/beta_test.go
@@ -1,21 +1,49 @@
 //
 //  Copyright (C) 2012-2017, Duzy Chan <code@duzy.info>, all rights reserverd.
 //
-package examples
+package smartest
 
 import (
+        "io/ioutil"
         "testing"
+        "bytes"
         "fmt"
         "os"
 )
 
-func TestCheckPrecond(t *testing.T) {
-        fmt.Fprintf(os.Stderr, "TODO: check preconditions\n")
-}
-func TestCheckResults(t *testing.T) {
-        fmt.Fprintf(os.Stderr, "TODO: check results\n")
+var wd, _ = os.Getwd()
+
+func TestCheckFiles(t *testing.T) {
+        if _, err := os.Stat("hello"); err != nil { t.Error(err) }
+        if _, err := os.Stat("hello.o"); err != nil { t.Error(err) }
+        if _, err := os.Stat("hello.cpp"); err != nil { t.Error(err) }
 }
 
-func TestInternal(t *testing.T) {
-        fmt.Fprintf(os.Stderr, "TODO: check internal\n")
+func TestCheckLog1(t *testing.T) {
+        if bv, err := ioutil.ReadFile("1.log"); err != nil { t.Error(err) } else {
+                if s, a, b, h, f := extractTextOutput(bv); a != nil && bytes.Equal(a, b) {
+                        var v = []byte(wd)
+                        if !bytes.Equal(v, a) { t.Errorf("bad path:\n%s\n%s", v, a) }
+
+                        v = []byte(fmt.Sprintf(``))
+                        if !bytes.Equal(v, h) { t.Errorf("bad header:\n%s\n%s", v, h) }
+
+                        v = []byte(fmt.Sprintf(`update file 'hello.cpp' ... (ok)
+g++ -std=c++1z -c hello.cpp -o hello.o
+g++ -std=c++1z hello.o   -o hello
+./hello
+Hello World!
+`))
+                        if !bytes.Equal(v, s) { t.Errorf("bad output:\n%s\n%s", v, s) }
+
+                        v = []byte(``)
+                        if !bytes.Equal(v, f) { t.Errorf("bad footer:\n%s\n%s", v, f) }
+                } else { t.Errorf("path: '%s' != '%s'", a, b) }
+        }
+}
+
+func TestCheckLog2(t *testing.T) {
+        if bv, err := ioutil.ReadFile("2.log"); err != nil { t.Error(err) } else {
+                if !bytes.Equal(bv, nil) { t.Errorf("bad output:\n%s", bv) }
+        }
 }
