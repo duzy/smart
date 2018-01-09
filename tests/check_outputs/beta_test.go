fc001aa6ec892f2532ef9b84e9749c47a471ca9e
Duzy Chan <code@duzy.info>
2018-01-09 22:55:15 +0800

    Fixed check_outputs test errors

--- tests/check_outputs/beta_test.go
+++ tests/check_outputs/beta_test.go
@@ -7,24 +7,37 @@ import (
         "testing"
         "io/ioutil"
         "bytes"
-        //"fmt"
+        "fmt"
+        "os"
 )
+
+var wd, _ = os.Getwd()
 
 func TestCheckLog1(t *testing.T) {
         if bv, err := ioutil.ReadFile("1.log"); err != nil { t.Error(err) } else {
-                if s, a, b := extractTextOutput(bv); a != nil && bytes.Equal(a, b) {
-                        v := []byte(`exit 123
+                if s, a, b, h, f := extractTextOutput(bv); a != nil && bytes.Equal(a, b) {
+                        var v = []byte(wd)
+                        if !bytes.Equal(v, a) { t.Errorf("bad path:\n%s\n%s", v, a) }
+
+                        v = []byte(fmt.Sprintf(``))
+                        if !bytes.Equal(v, h) { t.Errorf("bad header:\n%s\n%s", v, h) }
+
+                        v = []byte(fmt.Sprintf(`exit 123
 echo -n "okay"
 echo -n "okay" > /dev/stderr
 done check (okay)
-`)
-                        if !bytes.Equal(s, v) { t.Errorf("bad output:\n%s", bv) }
+`))
+                        if !bytes.Equal(v, s) { t.Errorf("bad output:\n%s\n%s", v, s) }
+
+                        v = []byte(``)
+                        if !bytes.Equal(v, f) { t.Errorf("bad footer:\n%s\n%s", v, f) }
                 } else { t.Errorf("path: '%s' != '%s'", a, b) }
         }
 }
 
 func TestCheckLog2(t *testing.T) {
-        if bv, err := ioutil.ReadFile("2.log"); err != nil { t.Error(err) } else {
-                if !bytes.Equal(bv, nil) { t.Errorf("bad output:\n%s", bv) }
+        if v, err := ioutil.ReadFile("2.log"); err != nil { t.Error(err) } else {
+                s := []byte(``)
+                if !bytes.Equal(v, s) { t.Errorf("bad output:\n%s\n%s", v, s) }
         }
 }
