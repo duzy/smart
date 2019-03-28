08815199619413f3a7207c404c16912a3e487351
Duzy Chan <code@duzy.info>
2019-03-28 12:25:29 +0800

    Display more details when panic on something

--- loader.go
+++ loader.go
@@ -2025,6 +2025,11 @@ func (l *loader) ParseFile(filename string, src interface{}, mode Mode) (f *ast.
 		if e := recover(); e != nil {
 			// resume same panic if it's not a bailout
 			if _, ok := e.(bailout); !ok {
+                                fmt.Fprintf(stderr, "%s: panic %T\n", filename, e)
+                                if l.parser != nil && l.parser.file != nil {
+                                        position := l.parser.file.Position(l.pos)
+                                        fmt.Fprintf(stderr, "%s: parsing fail\n", position)
+                                }
 				panic(e)
 			}
 		}
