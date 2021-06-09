5e36896b4921c280bedd10e43c526204b0c668e8
Duzy Chan <code@extbit.dev>
2021-06-09 15:48:50 +0800

    Remove the deprecated ast module

--- scope.go
+++ scope.go
@@ -7,42 +7,41 @@
 package smart
 
 import (
-        "extbit.io/smart/ast"
-        "strings"
-        "bytes"
-        "sync"
-        "sort"
-        "fmt"
-        "io"
+	"strings"
+	"bytes"
+	"sync"
+	"sort"
+	"fmt"
+	"io"
 )
 
 // A Scope maintains a set of objects;
 type Scope struct {
-        mutex *sync.Mutex
-        position Position
-        project *Project
-        outer *Scope
-        elems map[string]Object
-        comment string
+	mutex *sync.Mutex
+	position Position
+	project *Project
+	outer *Scope
+	elems map[string]Object
+	comment string
 }
 
 func NewScope(pos Position, outer *Scope, project *Project, comment string) *Scope {
-        scope := &Scope{
-                mutex: new(sync.Mutex),
-                position: pos,
-                project: project,
-                outer: outer,
-                elems: make(map[string]Object),
-                comment: comment,
-        }
-        return scope
+	scope := &Scope{
+		mutex: new(sync.Mutex),
+		position: pos,
+		project: project,
+		outer: outer,
+		elems: make(map[string]Object),
+		comment: comment,
+	}
+	return scope
 }
 
 func (s *Scope) copyElems() (result map[string]Object) {
-  s.mutex.Lock(); defer s.mutex.Unlock()
-  result = make(map[string]Object, len(s.elems))
-  for k, o := range s.elems { result[k] = o }
-  return
+	s.mutex.Lock(); defer s.mutex.Unlock()
+	result = make(map[string]Object, len(s.elems))
+	for k, o := range s.elems { result[k] = o }
+	return
 }
 
 func (s *Scope) Comment() string { return s.comment }
@@ -55,7 +54,7 @@ func (s *Scope) Len() int { return len(s.elems) }
 
 // Names returns the scope's element names in sorted order.
 func (s *Scope) Names() []string {
-  s.mutex.Lock(); defer s.mutex.Unlock()
+	s.mutex.Lock(); defer s.mutex.Unlock()
 	names := make([]string, len(s.elems))
 	i := 0
 	for name := range s.elems {
@@ -72,9 +71,9 @@ func (s *Scope) Names() []string {
 // Lookup returns the object in scope s with the given name if such an
 // object exists; otherwise the result is nil.
 func (s *Scope) Lookup(name string) (obj Object) {
-        s.mutex.Lock()
-        defer s.mutex.Unlock()
-        obj, _ = s.elems[name]; return
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	obj, _ = s.elems[name]; return
 }
 
 // findouter follows the outer chain of scopes starting with s until
@@ -87,35 +86,33 @@ func (s *Scope) Lookup(name string) (obj Object) {
 // time (see Insert, below). This can only happen for dot-imported objects
 // whose scope is the scope of the package that exported them.
 func (s *Scope) findouter(name string) (*Scope, Object) {
-        if false {
-                for p := s; p != nil; p = p.outer {
-                        if obj := p.Lookup(name); obj != nil /*&& (!pos.IsValid() || obj.scopePos() <= pos)*/ {
-                                return p, obj
-                        }
-                }
-        } else {
-                if s.outer != nil {
-                        if p, obj := s.outer.Find(name); obj != nil {
-                                return p, obj
-                        }
-                }
-        }
+	if false {
+		for p := s; p != nil; p = p.outer {
+			if obj := p.Lookup(name); obj != nil /*&& (!pos.IsValid() || obj.scopePos() <= pos)*/ {
+				return p, obj
+			}
+		}
+	} else {
+		if s.outer != nil {
+			if p, obj := s.outer.Find(name); obj != nil {
+				return p, obj
+			}
+		}
+	}
 	return nil, nil
 }
 
 func (s *Scope) Find(name string) (*Scope, Object) {
-        if obj := s.Lookup(name); obj == nil {
-                return s.findouter(name)
-        } else {
-                return s, obj
-        }
+	if obj := s.Lookup(name); obj == nil {
+		return s.findouter(name)
+	} else {
+		return s, obj
+	}
 }
 
-func (s *Scope) Resolve(name string) (sym ast.Symbol) {
-        if _, obj := s.Find(name); obj != nil {
-                sym = obj.(ast.Symbol)
-        }
-        return
+func (s *Scope) Resolve(name string) (obj Object) {
+	_, obj = s.Find(name)
+	return
 }
 
 // Insert attempts to insert an object obj into scope s.
@@ -124,19 +121,19 @@ func (s *Scope) Resolve(name string) (sym ast.Symbol) {
 // Otherwise it inserts obj, sets the object's outer scope
 // if not already set, and returns nil.
 func (s *Scope) Insert(obj Object) Object {
-        s.mutex.Lock()
-        defer s.mutex.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	name := obj.Name()
 	if alt := s.elems[name]; alt != nil {
 		return alt
 	}
-        s.replace(name, obj)
+	s.replace(name, obj)
 	return nil
 }
 
 func (s *Scope) replace(name string, obj Object) {
-        s.mutex.Lock()
-        defer s.mutex.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	if s.elems[name] = obj; obj.DeclScope() == nil {
 		obj.redecl(s)
 	}
@@ -147,8 +144,8 @@ func (s *Scope) replace(name string, obj Object) {
 // The level of indentation is controlled by n >= 0, with
 // n == 0 for no indentation.
 func (s *Scope) WriteTo(w io.Writer, n int) {
-        s.mutex.Lock()
-        defer s.mutex.Unlock()
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	const ind = ".  "
 	indn := strings.Repeat(ind, n)
@@ -176,77 +173,77 @@ func (s *Scope) String() string {
 }
 
 func (s *Scope) FindDef(name string) (def *Def) {
-        if _, sym := s.Find(name); sym != nil {
-                def, _ = sym.(*Def)
-        }
-        return
+	if _, sym := s.Find(name); sym != nil {
+		def, _ = sym.(*Def)
+	}
+	return
 }
 
 func (scope *Scope) ProjectName(owner *Project, name string, project *Project) (pn *ProjectName, alt Object) {
-        if alt = scope.elems[name]; alt == nil {
-                pn = &ProjectName{
-                        knownobject{
-                                objbase{
-                                        scope: scope,
-                                        owner: owner,
-                                }, name,
-                        },
-                        project,
-                }
-                scope.replace(name, pn)
-        }
-        return
+	if alt = scope.elems[name]; alt == nil {
+		pn = &ProjectName{
+			knownobject{
+				objbase{
+					scope: scope,
+					owner: owner,
+				}, name,
+			},
+			project,
+		}
+		scope.replace(name, pn)
+	}
+	return
 }
 
 func (scope *Scope) ScopeName(owner *Project, name string, s *Scope) (sn *ScopeName, alt Object) {
-        if alt = scope.elems[name]; alt == nil {
-                sn = &ScopeName{
-                        knownobject{
-                                objbase{
-                                        scope: scope,
-                                        owner: owner,
-                                }, name,
-                        },
-                        s,
-                }
-                scope.replace(name, sn)
-        }
-        return
+	if alt = scope.elems[name]; alt == nil {
+		sn = &ScopeName{
+			knownobject{
+				objbase{
+					scope: scope,
+					owner: owner,
+				}, name,
+			},
+			s,
+		}
+		scope.replace(name, sn)
+	}
+	return
 }
 
 func (scope *Scope) define(owner *Project, name string, value Value) (def *Def, alt Object) {
-        var okay bool
-        if alt, okay = scope.elems[name]; okay && alt == nil {
-                delete(scope.elems, name)
-                okay = false
-        }
-        if !okay {
-                def = &Def{
-                        knownobject{
-                                objbase{
-                                        scope: scope,
-                                        owner: owner,
-                                }, name,
-                        },
-                        DefDefault, value,
-                }
-                scope.replace(name, def)
-        }
-        return
+	var okay bool
+	if alt, okay = scope.elems[name]; okay && alt == nil {
+		delete(scope.elems, name)
+		okay = false
+	}
+	if !okay {
+		def = &Def{
+			knownobject{
+				objbase{
+					scope: scope,
+					owner: owner,
+				}, name,
+			},
+			DefDefault, value,
+		}
+		scope.replace(name, def)
+	}
+	return
 }
 
 func (scope *Scope) Builtin(name string, f BuiltinFunc) (bui *Builtin, alt Object) {
-        if alt = scope.elems[name]; alt == nil {
-                bui = &Builtin{
-                        knownobject{
-                                objbase{
-                                        scope: scope,
-                                        owner: nil,
-                                }, name,
-                        },
-                        builtinFlag(0), f,
-                }
-                scope.replace(name, bui)
-        }
-        return
+	if alt = scope.elems[name]; alt == nil {
+		bui = &Builtin{
+			knownobject{
+				objbase{
+					scope: scope,
+					owner: nil,
+				}, name,
+			},
+			builtinFlag(0), f,
+		}
+		scope.replace(name, bui)
+	}
+	return
 }
